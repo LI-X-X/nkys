0acdf98811e34c3c8381523af10e0621cbf3a9bf
Zhengxi Li <zhengxi.li.19@neoma-bs.com>
2019-07-09 00:20:18 +0200

    First winform application: 1) create the model of the table in database 2) create the dbcontext to manipulate the database 3) test the department can show in the datagrid

---
+++ nkys/ManagementApp/ManagementApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagementApp
+{
+    public partial class Form1 : Form
+    {
+        managementEntities db = new managementEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var result = (from d in db.Department
+                         select new {
+                             部门名 = d.DepartmentName,
+                             是否为工作车间 = d.IsWorkingAltelier
+                         }).ToList();
+            dataGridView1.DataSource = result;
+
+                      
+        }
+    }
+}
